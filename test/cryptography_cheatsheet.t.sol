251d806cb47e75193155c8da51e8ede23b1a888f
atharva <atharvsan3@gmail.com>
2025-02-16 09:22:02 +0530

    basic notes added

---
+++ test/cryptography_cheatsheet.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {cryptography_cheatsheet} from "../src/cryptography_cheatsheet.sol";
+
+contract cryptography_cheatsheetTest is Test {
+    cryptography_cheatsheet public c;
+
+    function setUp() public {
+        c = new cryptography_cheatsheet();
+    }
+
+}
